c86f10c0501f0d662793946ae0987399587a8828
Xwilarg <xwilarg@yahoo.fr>
2021-09-28 22:27:06 +0200

    Add input field

--- VNotify.Application/ViewModels/MainWindowViewModel.cs
+++ VNotify.Application/ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,28 @@
 using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Linq;
 using VNotify.Common.IO;
 
 namespace VNotify.Application.ViewModels
 {
     public class MainWindowViewModel : ReactiveObject
     {
-        public MainWindowViewModel()
-        {
-            _data = SaveData.Load();
-            _config = Configuration.Load();
-        }
-
         public bool IsApiKeyLoaded()
-            => _data.ApiKey != null;
+            => SaveData.Load().ApiKey != null;
 
         public bool AreChannelDataLoaded()
-            => _config.Channels.Length != 0;
-
-        private readonly SaveData _data;
-        private readonly Configuration _config;
+            => Configuration.Load().Channels.Length != 0;
 
         public string TrayReady { get; } = "The tray is now ready to be run";
+
+        public string VtuberName
+        {
+            set
+            {
+                DisplaySuggestions.Handle(value).GetAwaiter().GetResult();
+            }
+        }
+
+        public Interaction<string, Unit> DisplaySuggestions { get; } = new();
     }
 }
--- VNotify.Application/Views/MainWindow.axaml.cs
+++ VNotify.Application/Views/MainWindow.axaml.cs
@@ -1,7 +1,16 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
 using VNotify.Application.ViewModels;
+using VNotify.Common.IO;
 
 namespace VNotify.Application.Views
 {
@@ -13,6 +22,8 @@ namespace VNotify.Application.Views
 
             this.WhenActivated(_ =>
             {
+                ViewModel!.DisplaySuggestions.RegisterHandler(DisplaySuggestions);
+
                 if (!ViewModel!.IsApiKeyLoaded()) // API key is not loaded yet
                 {
                     var vm = new ApiKeyWindowViewModel();
@@ -36,6 +47,69 @@ namespace VNotify.Application.Views
                     }.Show(this);
                 }
             });
+        }
+
+        public void DisplaySuggestions(InteractionContext<string, Unit> context)
+        {
+            var input = context.Input.ToUpperInvariant();
+
+            var panel = this.FindControl<StackPanel>("Suggestions");
+            List<IControl> controls = new();
+            foreach (var channel in Configuration.Load().Channels
+                .OrderBy(x => {
+                    var enDistance = LevenshteinDistance(input, x.english_name?.ToUpperInvariant());
+                    var jpDistance = LevenshteinDistance(input, x.name?.ToUpperInvariant());
+                    return Math.Min(enDistance, jpDistance);
+                })
+                .Take(5)
+            )
+            {
+                var button = new Button
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Width = 300,
+                    Content = channel.english_name ?? channel.name
+                };
+                controls.Add(button);
+            }
+            panel.Children.Clear();
+            panel.Children.AddRange(controls);
+
+            context.SetOutput(Unit.Default);
+        }
+
+        // https://gist.github.com/Davidblkx/e12ab0bb2aff7fd8072632b396538560
+        private static int LevenshteinDistance(string source1, string source2)
+        {
+            if (source1 == null || source2 == null)
+                return int.MaxValue;
+
+            var source1Length = source1.Length;
+            var source2Length = source2.Length;
+
+            var matrix = new int[source1Length + 1, source2Length + 1];
+
+            if (source1Length == 0)
+                return source2Length;
+
+            if (source2Length == 0)
+                return source1Length;
+
+            for (var i = 0; i <= source1Length; matrix[i, 0] = i++) { }
+            for (var j = 0; j <= source2Length; matrix[0, j] = j++) { }
+
+            for (var i = 1; i <= source1Length; i++)
+            {
+                for (var j = 1; j <= source2Length; j++)
+                {
+                    var cost = (source2[j - 1] == source1[i - 1]) ? 0 : 1;
+
+                    matrix[i, j] = Math.Min(
+                        Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                        matrix[i - 1, j - 1] + cost);
+                }
+            }
+            return matrix[source1Length, source2Length];
         }
     }
 }
